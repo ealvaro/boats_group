d52ff0a07333b7b5ce9314c30676125c63508c56
ealvaro <alvaroescobaronline@gmail.com>
2017-10-28 22:22:09 -0400

    Added Array and getter for Class

--- Question1.java
+++ Question1.java
@@ -4,20 +4,35 @@ import java.util.*;
 
 public class Question1 {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for(int i=1; i <= n; i++){
-          if (i%3 == 0)
-            if (i%5 == 0)
-              System.out.println("BoatsGroup");
-            else
-              System.out.println("Boats");
-          else
-            if (i%5 == 0)
-              System.out.println("Group");
-            else
-              System.out.println(i);
-        }
+  private String[] arr;
+
+  public String[] getArr(){
+    return arr;
+  }
+
+  Question1(int n){
+    arr = new String[n];
+    for(int i=1; i <= n; i++){
+      if (i%3 == 0)
+        if (i%5 == 0)
+          arr[i-1]="BoatsGroup";
+        else
+          arr[i-1]="Boats";
+      else
+        if (i%5 == 0)
+          arr[i-1]="Group";
+        else
+          arr[i-1]=String.format("%s",i);
     }
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    Question1 q = new Question1(n);
+    String[] arr = q.getArr();
+    for(int i=0; i<n; i++){
+      System.out.println(arr[i]);
+    }
+  }
 }
